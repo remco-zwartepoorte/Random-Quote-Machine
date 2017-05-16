2759fd48b65fb1e011696339f75ee9215b36dd60
Remco Zwartepoorte <remco.zwartepoorte@gmail.com>
2017-05-16 14:40:44 +0200

    changed api url to https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getQuote() {
     $.ajax({
-        url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", success: function (json) {
+        url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", success: function (json) {
             //console.log(json);
             var post = json.shift();
             $("#quote-text").html(post.content);
